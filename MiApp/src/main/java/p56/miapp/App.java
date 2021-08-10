7b691a6c76b125438e91508d04d61c6dd35aa28c
Camilo A <ing_camiloacg@hotmail.com>
2021-08-10 17:01:49 -0500

    first commit

---
+++ MiApp/src/main/java/p56/miapp/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p56.miapp;
+
+/**
+ *
+ * @author Camilo A
+ */
+public class App {
+    
+    public static void main(String arg[]){
+        System.out.println("Hola a todos en Netbeans!!!");
+    }
+
+}
+
